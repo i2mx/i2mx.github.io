8edeb9c0483a127a33615ffbd60e9d6a50916a17
i2mx <i2mx@protonmail.com>
2024-06-29 17:31:42 +1200

    updated ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,30 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-100": "#ff9eef",
+        "primary-200": "#ffa9f1",
+        "primary-300": "#ffb5f3",
+        "primary-400": "#ffbff5",
+        "primary-500": "#ffcaf6",
+        "primary-600": "#ffd5f8",
+        "surface-100": "#1a1426",
+        "surface-200": "#2f293b",
+        "surface-300": "#464050",
+        "surface-400": "#5e5867",
+        "surface-500": "#77727e",
+        "surface-600": "#908c97",
+        "surface-mixed-100": "#412c49",
+        "surface-mixed-200": "#54405b",
+        "surface-mixed-300": "#68566e",
+        "surface-mixed-400": "#7c6c81",
+        "surface-mixed-500": "#918395",
+        "surface-mixed-600": "#a69aaa",
+        color: "#ffd5f8",
+        background: "#464050",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
-}
+};
